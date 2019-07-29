a6161f17d5c60d4dd3bc951aebde30663725ea65
Tobias Gerschner <tobias@deepthought.local>
2019-07-30 10:16:33 +1200

    save wip of cleaning up setting consumer config  optionally at runtime while respecting config set in config :kaffe

--- lib/kaffe/config/consumer.ex
+++ lib/kaffe/config/consumer.ex
@@ -1,44 +1,36 @@
 defmodule Kaffe.Config.Consumer do
   import Kaffe.Config, only: [heroku_kafka_endpoints: 0, parse_endpoints: 1]
 
-  def configuration(options \\ %{}) do
-    Map.merge(default_configuration(), options)
+  def configuration(options \\ %{})
+  def configuration(options) do
+    Map.merge(default_configuration(),options)
   end
-
   def default_configuration() do
+    default_config =
     %{
       endpoints: endpoints(),
-      subscriber_name: subscriber_name(),
-      consumer_group: config_get(:consumer_group, nil),
-      topics: config_get(:topics, []),
-      group_config: consumer_group_config(),
+      subscriber_name: nil,
+      consumer_group: nil,
+      topics: [],
+      group_config: [
+        offset_commit_policy: :commit_to_kafka_v2,
+        offset_commit_interval_seconds: 5
+      ],
       consumer_config: client_consumer_config(),
-      message_handler: message_handler(),
-      async_message_ack: async_message_ack(),
-      rebalance_delay_ms: rebalance_delay_ms(),
-      max_bytes: max_bytes(),
-      min_bytes: min_bytes(),
-      max_wait_time: max_wait_time(),
-      subscriber_retries: subscriber_retries(),
-      subscriber_retry_delay_ms: subscriber_retry_delay_ms(),
-      offset_reset_policy: offset_reset_policy(),
-      worker_allocation_strategy: worker_allocation_strategy()
+      message_handler: nil,
+      async_message_ack: false,
+      rebalance_delay_ms: 10_000,
+      max_bytes: 1_000_000,
+      min_bytes: 0,
+      max_wait_time: 10_000,
+      subscriber_retries: 5,
+      subscriber_retry_delay_ms: 5_000,
+      offset_reset_policy: :reset_by_subscriber,
+      worker_allocation_strategy: :worker_per_partition
     }
+    kaffe_config = Application.get_env(:kaffe, :consumer) |> Enum.into(%{})
+    Map.merge(default_config, kaffe_config)
   end
-
-
-  def subscriber_name do
-    case config_get(:subscriber_name, nil) do
-      nil ->
-        nil
-      configured_subscriber_name ->
-        String.to_atom(configured_subscriber_name)
-    end
-  end
-
-  def message_handler, do: config_get!(:message_handler)
-
-  def async_message_ack, do: config_get(:async_message_ack, false)
 
   def endpoints do
     if heroku_kafka?() do
@@ -46,37 +38,6 @@ defmodule Kaffe.Config.Consumer do
     else
       parse_endpoints(config_get!(:endpoints))
     end
-  end
-
-  def consumer_group_config do
-    [
-      offset_commit_policy: :commit_to_kafka_v2,
-      offset_commit_interval_seconds: config_get(:offset_commit_interval_seconds, 5)
-    ]
-  end
-
-  def rebalance_delay_ms do
-    config_get(:rebalance_delay_ms, 10_000)
-  end
-
-  def max_bytes do
-    config_get(:max_bytes, 1_000_000)
-  end
-
-  def min_bytes do
-    config_get(:min_bytes, 0)
-  end
-
-  def max_wait_time do
-    config_get(:max_wait_time, 10_000)
-  end
-
-  def subscriber_retries do
-    config_get(:subscriber_retries, 5)
-  end
-
-  def subscriber_retry_delay_ms do
-    config_get(:subscriber_retry_delay_ms, 5_000)
   end
 
   def client_consumer_config do
@@ -108,14 +69,6 @@ defmodule Kaffe.Config.Consumer do
       true -> :earliest
       false -> -1
     end
-  end
-
-  def offset_reset_policy do
-    config_get(:offset_reset_policy, :reset_by_subscriber)
-  end
-
-  def worker_allocation_strategy do
-    config_get(:worker_allocation_strategy, :worker_per_partition)
   end
 
   def maybe_heroku_kafka_ssl do
